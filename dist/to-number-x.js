2f8d2da45fbaff718a2d633bb8e7df364e85dcc5
Graham Fairweather <xotic750@gmail.com>
2019-07-18 01:35:28 +0200

    :bookmark: v3.0.15
    Update dependencies

--- dist/to-number-x.js
+++ dist/to-number-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-17T18:29:28.783Z",
+  "date": "2019-07-17T23:35:22.215Z",
   "describe": "",
   "description": "Converts argument to a value of type Number.",
   "file": "to-number-x.js",
   "hash": "7784ddb1d9ef5474bed6",
   "license": "MIT",
-  "version": "3.0.14"
+  "version": "3.0.15"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
